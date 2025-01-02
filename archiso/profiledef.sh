f65b0156fe0c9ef81237d6e7c27d584dbc76d5f5
eshanized <m.eshanized@gmail.com>
2025-01-02 13:39:55 +0530

    🧹 chore: upgrade the permssions and files

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -17,12 +17,13 @@ airootfs_image_type="squashfs"
 airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=('zstd' '-c' '-T0' '--auto-threads=logical' '--long' '-19')
 file_permissions=(
+  ["/etc/gshadow"]="0:0:600"
   ["/etc/shadow"]="0:0:400"
-  ["/root"]="0:0:750"
+  ["/root"]="0:0:700"
   ["/root/.automated_script.sh"]="0:0:755"
-  ["/root/.gnupg"]="0:0:700"
-  ["/usr/local/bin/choose-mirror"]="0:0:755"
-  ["/usr/local/bin/Installation_guide"]="0:0:755"
-  ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/etc/grub.d/40_custom"]="0:0:755"
+  ["/usr/local/bin/snigdhaos-snapper"]="0:0:755"
   ["/etc/NetworkManager/dispatcher.d/09-timezone"]="0:0:755"
 )
